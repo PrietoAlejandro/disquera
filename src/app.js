63ca38843ca197ca5142c4ebae9b5e28c0785af8
PrietoAlejandro <faospitia2@misena.edu.co>
2022-11-23 22:49:30 -0500

    evidencia

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require("cors");
+const morgan = require('morgan');
+const conexionBD = require('./db.conexion');
+const rutasHabitacion = require('./routes/habitaciones.routes');
+const rutasUser=require('./routes/users.routes');
+const app = express()
+const port= process.env.PORT || 3000;
+//Conexión a la BD
+conexionBD();
+
+//Configuraciones
+app.set("name","disquera");
+app.set("port",process.env.port || 3500);
+
+//Midlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+//Llamado de rutas
+app.use('/public', express.static('public/upload'));
+//app.use('/public', express.static(__dirname + '/public'));
+app.use("/api/habitaciones",rutasHabitacion);
+app.use("/api/users",rutasUser);
+
+
+module.exports=app;
